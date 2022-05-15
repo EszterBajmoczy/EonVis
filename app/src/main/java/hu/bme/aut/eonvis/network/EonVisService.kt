869513aa38258155e7cca2053e67639fdeae1d83
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-05-15 17:33:39 +0200

    Network layer

--- app/src/main/java/hu/bme/aut/eonvis/network/EonVisService.kt
+++ app/src/main/java/hu/bme/aut/eonvis/network/EonVisService.kt
@@ -1,19 +1,109 @@
 package hu.bme.aut.eonvis.network
 
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import hu.bme.aut.eonvis.data.model.PowerConsume
 import javax.inject.Inject
 
 
 class EonVisService @Inject constructor()  {
     private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var db = FirebaseFirestore.getInstance()
+
+    private var userId: String? = null
+
+    init {
+        userId = mAuth.currentUser?.uid
+    }
 
     fun login(username: String, password: String, loginCallback: (Boolean) -> Unit) {
         mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener { task ->
             loginCallback(task.isSuccessful)
+            userId = mAuth.currentUser?.uid
         }
     }
 
     fun isLoggedIn(callback: (Boolean) -> Unit) {
         callback(mAuth.currentUser != null)
+    }
+
+    fun getPowerConsumes(callback: (List<PowerConsume>) -> Unit) {
+        db.collection(userId!!).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val list = ArrayList<PowerConsume>()
+                for (firebaseData in task.result!!.documents) {
+                    try {
+                        val data = PowerConsume(
+                            id = firebaseData.getLong("id")!!,
+                            incoming = firebaseData.getDouble("incoming")!!,
+                            outgoing = firebaseData.getDouble("outgoing")!!)
+                        firebaseData.reference.collection("tags")
+                            .document("tags")
+                            .get()
+                            .addOnCompleteListener { tagData ->
+                                val tagList = ArrayList<String>()
+                                tagData.result.data?.values?.forEach { tag -> tagList.add(tag.toString()) }
+                                data.addTags(tagList)
+                            }
+                        list.add(data)
+                    } catch (e: Exception){
+                        Log.d("Firebase", "Invalid data format.")
+                    }
+                }
+                callback(list)
+            } else {
+                Log.d("Firebase", "No data for user")
+            }
+        }
+    }
+
+    fun addTag(id: Long, tag: String, callback: (Boolean) -> Unit) {
+        val data: MutableMap<String, Any> = HashMap()
+        data[tag] = tag
+
+        db.collection(userId!!).document(id.toString()).collection("tags").document("tags")
+            .update(data).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                callback(true)
+            } else {
+                callback(false)
+            }
+        }
+    }
+
+    fun removeTag(id: Long, tag: String, callback: (Boolean) -> Unit) {
+        val deleteTag: MutableMap<String, Any> = HashMap()
+        deleteTag[tag] = FieldValue.delete()
+
+        db.collection(userId!!).document(id.toString()).collection("tags").document("tags").update(deleteTag).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                callback(true)
+            } else {
+                callback(false)
+            }
+        }
+    }
+
+    fun getTags(callback: (List<String>) -> Unit) {
+        db.collection(userId!!)
+            .get()
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    for (dataDoc in task.result!!.documents) {
+                        dataDoc.reference.collection("tags")
+                            .document("tags")
+                            .get()
+                            .addOnCompleteListener { data ->
+                                val list = ArrayList<String>()
+                                data.result.data?.values?.forEach { tag -> list.add(tag.toString()) }
+                                callback(list)
+                        }
+                    }
+                } else {
+                    Log.d("Firebase", "Could not get documents.")
+                }
+            }
     }
 }
